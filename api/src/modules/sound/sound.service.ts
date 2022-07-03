5b4947e2b82d984fd75af3fd04be7e37e8fb4c91
BEUZELIN Charly <ext.cbeuzelin@cma-cgm.com>
2022-07-03 15:00:32 +0200

    WIP refacto

--- api/src/modules/sound/sound.service.ts
+++ api/src/modules/sound/sound.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import * as fs from 'fs';
 
 import { Sound, SoundDocument } from './resources/schemas/sound.schema';
 import { SoundPost } from './resources/classes/sound.class';
 import { User } from '../user/resources/schemas/user.schema';
 import { ISoundFiles } from './resources/interfaces/sound.interface';
-import { EFileType } from './resources/enums/sound.enum';
+import { StorageUtils } from '../../utils/storageUtils';
 
 @Injectable()
 export class SoundService {
@@ -15,47 +14,8 @@ export class SoundService {
     @InjectModel(Sound.name) private soundModel: Model<SoundDocument>,
   ) {}
 
-  findSounds(): Promise<Sound[]> {
+  public findSounds(): Promise<Sound[]> {
     return this.soundModel.find().exec();
-  }
-
-  private storeFiles(files: ISoundFiles, soundId: string) {
-    const types = [EFileType.AUDIO, EFileType.IMAGE];
-
-    const promises = types.map((type) => {
-      return new Promise<void>((resolve, reject) => {
-        const dir = `${process.env.UPLOADS_DIRECTORY}/${type}`;
-        const path = `${dir}/${soundId}`;
-
-        fs.mkdir(dir, { recursive: true }, (err) => {
-          if (err) reject(err);
-
-          fs.writeFile(path, files[type][0].buffer, (err) => {
-            if (err) reject(err);
-            resolve();
-          });
-        });
-      });
-    });
-
-    return Promise.all(promises);
-  }
-
-  private removeFiles(soundId: string): Promise<void[]> {
-    const formattedFiles = [EFileType.AUDIO, EFileType.IMAGE];
-
-    const promises = formattedFiles.map((type) => {
-      return new Promise<void>((resolve, reject) => {
-        const path = `${process.env.UPLOADS_DIRECTORY}/${type}/${soundId}`;
-
-        fs.unlink(path, (err) => {
-          if (err) reject(err);
-          resolve();
-        });
-      });
-    });
-
-    return Promise.all(promises);
   }
 
   createSound(
@@ -65,13 +25,15 @@ export class SoundService {
   ): Promise<Sound> {
     const newSound = new this.soundModel({ ...sound, author });
     return newSound.save().then((sound) => {
-      return this.storeFiles(files, sound._id).then(() => sound);
+      return StorageUtils.storeFiles(files, sound._id).then(() => sound);
     });
   }
 
-  deleteSound(id: string): Promise<void[]> {
-    return this.soundModel
-      .findOneAndDelete({ _id: id })
-      .then(() => this.removeFiles(id));
+  archiveSound(id: string): Promise<void[]> {
+    this.soundModel.findOneAndUpdate({ _id: id }, { isArchived: true });
+
+    return StorageUtils.archiveFiles(id).then(() =>
+      this.soundModel.findOneAndDelete({ _id: id }),
+    );
   }
 }
